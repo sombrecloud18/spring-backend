bef9a75f9cedb74a9d2934c03de2da72a9934cc1
Kate <billykatessss@gmail.com>
2025-08-04 21:13:14 +0300

    fix: fixed authRepo

--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -1,1 +1,9 @@
 import { pool } from '../db.js';
+
+export const findUser = async (username, password)  => {
+    const { rows } = await pool.query(
+      'SELECT * FROM users WHERE username = $1 AND password = $2',
+      [username, password]
+    );
+    return rows[0] || null;
+}
